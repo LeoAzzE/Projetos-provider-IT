67afcde2df3ef71a40158187678acb18a5741f27
Borges dev <leosull50@hotmail.com>
2023-03-24 01:50:46 -0300

    implementing API VIA CEP

--- ficha cadastral com JS/script.js
+++ ficha cadastral com JS/script.js
@@ -8,19 +8,18 @@ var casado = document.getElementById('casado')
 var viuvo = document.getElementById('viuvo')
 var sexo = document.querySelector('#sexo')
 var button = document.getElementById('buttonClean')
-var sc = document.querySelector("input[name='sc']:checked")
 var body = document.getElementById('body')
+var cep = document.getElementById('cep')
 var radioElements = [solteiro, casado, viuvo]
 var userInformation = [userName, userRg, userCpf, userAdress, userBorn]
-
 array = []
 
 var db = openDatabase('mydb', '1.0', 'cadastro de pessoas', 2 * 1024 * 1024)
-  db.transaction(function (tx) {
-    tx.executeSql(
-      'create table if not exists people(name, rg, cpf, end, nasc, sexo, sc)'
-      )
-    })
+db.transaction(function (tx) {
+  tx.executeSql(
+    'create table if not exists people(name, rg, cpf, end, nasc, sexo, sc)'
+  )
+})
 
 function cleanFields() {
   userInformation.forEach(field => {
@@ -35,42 +34,35 @@ function cleanFields() {
 button.addEventListener('click', cleanFields)
 buttonInclude.addEventListener('click', insertData)
 
-dataListOnScreen()
-function dataListOnScreen() {
-  db.transaction(function (tx) {
-    tx.executeSql(
-      'select * from people',
-      [],
-      function (tx, results) {
-        var n = results.rows.length,i
-        var str = ''
-        for (i = 0; i < n; i++) {
-          str += '<tr>'
-          str += '<td>' + results.rows.item(i).name + '</td>'
-          str += '<td>' + results.rows.item(i).rg + '</td>'
-          str += '<td>' + results.rows.item(i).cpf + '</td>'
-          str += '<td>' + results.rows.item(i).end + '</td>'
-          str += '<td>' + results.rows.item(i).nasc + '</td>'
-          str += '<td>' + results.rows.item(i).sexo + '</td>'
-          str += '<td>' + results.rows.item(i).sc + '</td>'
-          str += '</tr>'
-          document.getElementById('tbody').innerHTML += str
-          str = ''
-        }
-      },
-      null
-      )
-    }
-    )
-}
+db.transaction(function (tx) {
+  tx.executeSql(
+    'select * from people',
+    [],
+    function (tx, results) {
+      var n = results.rows.length,
+        i
+      var str = ''
+      for (i = 0; i < n; i++) {
+        str += '<tr>'
+        str += '<td>' + results.rows.item(i).name + '</td>'
+        str += '<td>' + results.rows.item(i).rg + '</td>'
+        str += '<td>' + results.rows.item(i).cpf + '</td>'
+        str += '<td>' + results.rows.item(i).end + '</td>'
+        str += '<td>' + results.rows.item(i).nasc + '</td>'
+        str += '<td>' + results.rows.item(i).sexo + '</td>'
+        str += '<td>' + results.rows.item(i).sc + '</td>'
+        str += '</tr>'
+        document.getElementById('tbody').innerHTML += str
+        str = ''
+      }
+    },
+    null
+  )
+})
 
 function insertData() {
-  event.preventDefault
-  let sCivil = radioElements.map((e)=> {
-    if(e.checked) {
-        return e.value
-    }
-  })
+  let sc = document.querySelector("input[name='sc']:checked")
+
   people = {
     name: userName.value,
     rg: userRg.value,
@@ -78,14 +70,42 @@ function insertData() {
     end: userAdress.value,
     nasc: userBorn.value,
     sexo: sexo.value,
-    situacaoCivil: sCivil
+    sc: sc.value
   }
   array.push(people)
   db.transaction(function (tx) {
-    for(let p of array) {
+    for (let p of array) {
       tx.executeSql(
         'insert into people(name, rg, cpf, end, nasc, sexo, sc) values (?, ?, ?, ?, ?, ?, ?)',
-        [p.name, p.rg, p.cpf, p.end, p.nasc, p.sexo, p.situacaoCivil]);
-      }
-    }) //window.location.reload(true)
+        [p.name, p.rg, p.cpf, p.end, p.nasc, p.sexo, p.sc]
+      )
+    }
+  }, window.location.reload())
+}
+
+cep.addEventListener("keyup", (e) => {
+  const inputValue = e.target.value
+
+  if (inputValue.length === 8) {
+    consultaEndereco(cep.value)
   }
+
+})
+
+const consultaEndereco = async (cep) => {
+
+    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
+
+    const response = await fetch(apiUrl)
+
+    const data = await response.json()
+
+    document.getElementById('bairro').value = data.bairro
+    document.getElementById('city').value = data.localidade
+    document.getElementById('adressInput').value = data.logradouro
+    document.getElementById('comp').value = data.document
+
+    if (document.getElementById('comp').value === 'undefined') {
+      document.getElementById('comp').value = ""
+    }
+}
