32099fe739d12d4ade291f9dd6446dbfb6269f90
Borges dev <leosull50@hotmail.com>
2023-04-23 13:43:47 -0300

    add tarefas

--- firebase/using-firebase/src/pages/Admin/index.js
+++ firebase/using-firebase/src/pages/Admin/index.js
@@ -1,14 +1,39 @@
 import './admin.css'
-import { useState } from 'react'
-import { auth } from '../../firebaseConnection'
+import { useState, useEffect } from 'react'
+import { auth, db } from '../../firebaseConnection'
 import { signOut } from 'firebase/auth'
+import {addDoc, collection} from 'firebase/firestore'
 
 export default function Admin() {
   const [tarefaInput, setTarefaInput] = useState('')
+  const [user, setUser] = useState('')
 
-  function handleRegister(e) {
+  useEffect(()=> {
+    async function loadTarefas() {
+      const userDetail = localStorage.getItem("@detailUser")
+      setUser(JSON.parse( userDetail))
+    }
+    loadTarefas()
+  }, [])
+
+  async function handleRegister(e) {
     e.preventDefault()
-    alert('clicou')
+    if (tarefaInput === '') {
+      alert("digite sua tarefa")
+      return 
+    }
+    await addDoc(collection(db,"tarefas"), {
+      tarefa: tarefaInput,
+      created: new Date(),
+      userUid: user?.uid
+    })
+    .then(()=> {
+      console.log('registrada')
+      setTarefaInput('')
+    })
+    .catch((error)=> {
+      console.log('error')
+    })
   }
 
   async function handleLogout() {
--- projeto filmes/projeto-filmes/src/pages/Filme/index.js
+++ projeto filmes/projeto-filmes/src/pages/Filme/index.js
@@ -66,7 +66,7 @@ function Filme() {
       />
       <h3>Sinopse</h3>
       <span>{filme.overview}</span>
-      <strong>Avaliação: {filme.vote_average} / 10</strong>
+      <strong>Avaliação: {filme.vote_average.toFixed(0)} / 10</strong>
 
       <div className="area-buttons">
         <button onClick={salvarFilme}>Salvar</button>
