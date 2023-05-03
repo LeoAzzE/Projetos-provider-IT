4efc869424782c6579550cbd83a585c71af3572a
Borges dev <leosull50@hotmail.com>
2023-05-03 00:53:35 -0300

    exibindo os dados do firebase na tabela

--- projeto-final-react/list/src/components/Modal.jsx
+++ projeto-final-react/list/src/components/Modal.jsx
@@ -2,65 +2,188 @@ import { useState } from 'react';
 import './Modal.css'
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import  validator  from "validator";
+import { useForm } from "react-hook-form"
+import { InputMask } from 'primereact/inputmask';
+import { Calendar } from 'primereact/calendar';
+import { RadioButton } from 'primereact/radiobutton';
+import { addDoc, collection} from 'firebase/firestore'
+import { toast } from 'react-toastify'
+import { db } from '../services/firebase'
 import 'primeicons/primeicons.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";     
 import "primereact/resources/primereact.min.css";
 
 
-export default function Modal(){
-    const [closeModal, setCloseModal] = useState(true)
+export default function Modal({close}){
+    const[userName, setUserName] = useState('')
+    const[userEmail, setUserEmail] = useState('')
+    const[cpf, setCpf] = useState('')
+    const[nasc, setNasc] = useState('')
+    const [cep, setCep] = useState('')
+    const[estado, setEstado] = useState(null)
+    const [log, setLog] = useState(null)
+    const [bairro, setBairro] = useState(null)
+    const [cidade, setCidade] = useState(null)
+    const [numero, setNumero] = useState(null)
+    const [gender, setGender] = useState(null)
+ 
 
-    function handleCloseModal() {
-        if(closeModal) {
-            setCloseModal(false)
+    const { register, handleSubmit, setFocus, setValue, formState: { errors }} = useForm()
+       
+    async function onSubmit(data) {
+        await addDoc(collection(db, "clientes") , {
+            nome: data.nameCad,
+            email: data.emailCad,
+            nascimento: data.nascCad,
+            cpf: data.cpfCad,
+            cep: data.cepCad,
+            endereco: data.estCad + ", " + data.cidadeCad + ", " + data.bairroCad + ", " + data.logCad + ", " + data.numeroCad,
+            gender: data.gender
+        })
+        .then(()=> {
+            toast.success('cliente cadastrado com sucesso')
+            window.location.reload()
+        })
+    } 
+    
+    function cpfFormatter(cpf) {
+        return cpf.replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
         }
-        else {
-            setCloseModal(true)
+
+    function cepFormatter(cep) {
+        return cep.replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1')
+    }
+
+    // function dateFormatter() {
+    //     let dataAtual = new Date()
+    //     let dia = String(dataAtual.getDate() - 1).padStart(2, '0')
+    //     let mes = String(dataAtual.getMonth() + 1).padStart(2, '0')
+    //     let ano = dataAtual.getFullYear()
+    //     let date = ano + '-' + mes + '-' + dia
+    //     return date
+    // }
+    
+    async function cepComplete(e) {
+        if (!e.target.value) {
+            return;
+        } else {
+
+            const cep = e.target.value.replace(/\D/g, '')
+            const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
+            const response = await fetch(apiUrl)
+            const data = await response.json()
+
+            setValue('estCad', data.uf)
+            setValue('cidadeCad', data.localidade)
+            setValue('logCad', data.logradouro)
+            setValue('bairroCad', data.bairro)
+            setFocus('numeroCad')
         }
+
+            
+
     }
 
     return (
-        <div className={(closeModal) ? "modall" : "none"}>
+        <div className="modall">
             <div className='container'>
                 <h1 className='aga1'>Cadastrar cliente</h1>
                 <span className='lineCad'></span>
-                <Button onClick={handleCloseModal} className='close' icon="pi pi-times" rounded outlined severity="danger" aria-label="Cancel" />
+                <Button onClick={close} className='close' icon="pi pi-times" rounded outlined severity="danger" aria-label="Cancel" />
                 <main className='inputTexts'>
-                    <div className='afastar'>
-                        <span className="p-float-label">
-                            <InputText size={15} id="username" />
-                            <label htmlFor="username">Nome</label>
-                        </span>
-                        <span className="p-float-label">
-                            <InputText size={15} id="username" />
-                            <label htmlFor="username">E-mail</label>
-                        </span>
-                    </div>
-                    <div className='afastar2'>
-                        <span className="p-float-label">
-                            <InputText size={15} id="username" />
-                            <label htmlFor="username">CPF</label>
-                        </span>
-                        <span className="p-float-label">
-                            <InputText size={15} id="username" />
-                            <label htmlFor="username">Nascimento</label>
-                        </span>
-                    </div>
-                    <div className='afastar3'>
-                        <span className="p-float-label">
-                            <InputText size={15} id="username" />
-                            <label htmlFor="username">Endereco</label>
-                        </span>
-                        <span className="p-float-label">
-                            <InputText size={15} id="username" />
-                            <label htmlFor="username">Gênero</label>
-                        </span>
-                    </div>
-                    <div className='btnCad'>
-                    <Button
-                    style={{backgroundColor: '#85bb65'}} severity='Sucess' size="normal" label="Cadastrar"/>
-                    </div>
-                </main>
+                        <div className='afastar'>
+                            <span className="cli">
+                                <label htmlFor="username">Nome</label>
+                                <InputText className={errors?.nameCad && "p-invalid"} {... register('nameCad', {required: true, minLength: 3})} value={userName} onChange={(e)=> setUserName(e.target.value)} size={15} id="username" />
+                                {errors?.nameCad?.type === "required" && (<p className='erro-msg'>O Nome é obrigatorio</p>)}
+                                {errors?.nameCad?.type === "minLength" && (<p className='erro-msg'>O Nome deve ter no minimo 3 caracteres</p>)}
+                            </span>
+                            <span className="cli">
+                                <label htmlFor="email">Email</label>
+                                <InputText className={errors?.emailCad && "p-invalid"} {... register('emailCad', {required: true, validate: (value) => validator.isEmail(value)})} value={userEmail} onChange={(e)=> setUserEmail(e.target.value)} size={15} id="useremail" />
+                                {errors?.emailCad?.type && (<p className='erro-msg'>Insira um e-mail válido</p>)}   
+                            </span>
+                            
+                            <span className="cli">
+                                <label htmlFor="username">Nascimento</label>
+                                <InputText type='date' className={errors?.nascCad && "p-invalid"} size={10} {... register('nascCad', {required: true})} value={nasc} onChange={(e)=> setNasc(e.target.value)} id="usernasc" />
+                                {errors?.nascCad?.type && (<p className='erro-msg'>Insira uma data válida</p>)} 
+                            </span>
+                            
+                        </div>
+                        <div className='afastar2'>
+                            <span className="cli">
+                                <label htmlFor="userCpf">CPF</label>
+                                <InputText maxLength={14} keyfilter="int" {... register('cpfCad', {required: true, minLength: 14})} className={errors?.cpfCad && "p-invalid"} size={13} value={cpfFormatter(cpf)} onChange={(e) => setCpf(e.target.value)}  placeholder="CPF" />
+                                {errors?.cpfCad?.type && (<p className='erro-msg'>Insira um cpf válido</p>)}
+                            </span>
+                            <span className="cli">
+                                <label htmlFor="userCep">CEP</label>
+                                <InputText maxLength={9} onKeyUp={cepComplete} {...register('cepCad', {required: true, minLength: 9})} className={errors?.cepCad && "p-invalid"}  size={13} onChange={(e)=> setCep(e.target.value)} value={cepFormatter(cep)} id="cep" />
+                                {errors?.cepCad?.type && (<p className='erro-msg'>Insira um cep válido</p>)} 
+                                
+                            </span>
+                            <span className="cli">
+                                <label htmlFor="username">Estado</label>
+                                <InputText className={errors?.estCad && "p-invalid"} size={15} {... register('estCad', {required: true,minLength: 2})} value={estado} onChange={(e)=> setEstado(e.target.value)} id="username" />
+                                {errors?.estCad?.type === "required" && (<p className='erro-msg'>O Estado é obrigatório</p>)}
+                                {errors?.estCad?.type === "minLength" && (<p className='erro-msg'>Mínimo 2 caracteres</p>)}
+                            </span>
+                        </div>
+                        <div className='afastar3'>
+                            <span className="cli">
+                                <label htmlFor="username">Cidade</label>
+                                <InputText className={errors?.cidadeCad && "p-invalid"} size={11} {... register('cidadeCad', {required: true,minLength: 2})} value={cidade} onChange={(e)=> setCidade(e.target.value)} id="username" />
+                                {errors?.cidadeCad?.type && (<p className='erro-msg'>A cidade é obrigatória</p>)}
+                                {errors?.cidadeCad?.type === "minLength" && (<p className='erro-msg'>Mínimo 2 caracteres</p>)}
+                            </span>
+                            <span className="cli">
+                                <label htmlFor="username">Bairro</label>
+                                <InputText className={errors?.bairroCad && "p-invalid"} size={12} {... register('bairroCad', {required: true})} id="username" value={bairro} onChange={(e)=> setBairro(e.target.value)} />
+                                {errors?.bairroCad?.type && (<p className='erro-msg'>O bairro é obrigatório</p>)} 
+                            </span>
+                            <span className="cli">
+                                <label htmlFor="username">Logradouro</label>
+                                <InputText className={errors?.logCad && "p-invalid"} {... register('logCad', {required: true, minLength: 2})} value={log} onChange={(e)=> setLog(e.target.value)} size={12} id="username" />
+                                {errors?.logCad?.type === "required" && (<p className='erro-msg'>mínimo 2 digitos</p>)}
+                                {errors?.logCad?.type === "minLength" && (<p className='erro-msg'>O Logradouro é obrigatório</p>)}
+                            </span>
+                            <span className="cli">
+                                <label htmlFor="username">N°</label>
+                                <InputText className={errors?.numeroCad && "p-invalid"} {... register('numeroCad', {required: true})} value={numero} onChange={(e)=> setNumero(e.target.value)} size={5} id="username" />
+                                {errors?.numeroCad?.type === "required" && (<p className='erro-msg'>O numero é obrigatório</p>)}
+                            </span>
+                        </div>
+
+                        <div className='genderContainer'>
+                            <div className='gender'>*Gênero:</div>
+                                <div className='separator'>
+                                    <div className='radioInputs'>
+                                        <input {... register('gender', {required: true})}
+                                        value='masc' type="radio" name="gender" id="" /> Masculino
+                                        <input {... register('gender', {required: true})} value='fem' type="radio" name="gender" id="" /> Feminino
+                                        <input {... register('gender', {required: true})} value='other' type="radio" name="gender" id="" /> Outro
+                                    </div>
+                                    <div className='error'>
+                                        {errors?.gender && <p className='erro-msg'>O gênero é obrigatório</p>}
+                                    </div>
+
+                            </div>
+                            
+                        </div>
+                        
+                        <div className='btnCad'>
+                        <Button onClick={()=> handleSubmit(onSubmit)()} type='submit'
+                        style={{backgroundColor: '#85bb65'}} severity='Sucess' size="normal" label="Cadastrar"/>
+                        </div>
+                    </main>    
             </div>
         </div>
     )
--- projeto-final-react/list/src/pages/MainPage/MainPage.jsx
+++ projeto-final-react/list/src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,4 @@
-import {useContext, useState} from 'react'
+import {useContext, useState, useEffect} from 'react'
 import { AuthContext } from '../../contexts/auth'
 import '../MainPage/MainPage.css'
 import { DataTable } from 'primereact/datatable';
@@ -10,10 +10,16 @@ import 'primeicons/primeicons.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";     
 import "primereact/resources/primereact.min.css";
 import  Modal from '../../components/Modal'
+import { collection, getDocs, query} from 'firebase/firestore'
+import { db } from '../../services/firebase'
+
+const listaRef = collection(db, "clientes")
 
 function MainPage() {
     const { logout } = useContext(AuthContext)
     const [showModal, setShowModal] = useState(false)
+    const [listaClientes, setListaClientes] = useState([])
+    const [isEmpty, setIsEmpty] = useState(false)
     const [filters, setFilters] = useState({
         global: {value: null, matchMode: FilterMatchMode.CONTAINS}
     })
@@ -22,14 +28,48 @@ function MainPage() {
         await logout()
     }
 
-    function handleOpenModal() {
-            if (!showModal) {
-                setShowModal(true)
+    function handleOpenModal() {   
+            setShowModal(true)
+        }
+
+        useEffect(() => {
+            async function loadLista() {
+                const q = query(listaRef)
+
+                const querySnapshot = await getDocs(q)
+                setListaClientes([])
+                await updateList(querySnapshot)
+            }
+
+            loadLista()
+
+            return () => {
+
+            }
+        },[])
+
+        async function updateList(querySnapshot) {
+            const isCollectionEmpty = querySnapshot.size === 0
+
+            if (!isCollectionEmpty) {
+                let lista = []
+
+                querySnapshot.forEach(doc => {
+                    lista.push({
+                        nome: doc.data().nome,
+                        email: doc.data().email,
+                        nascimento: doc.data().nascimento,
+                        cpf: doc.data().cpf,
+                        cep: doc.data().cep,
+                        endereco: doc.data().endereco,
+                        genero: doc.data().gender
+                    })
+                });
+                setListaClientes(clientes =>[...clientes, ...lista ])
             }
             else {
-                setShowModal(false)
+                
             }
-            
         }
 
     return (
@@ -46,23 +86,20 @@ function MainPage() {
                     }}/>
                     <i className="pi pi-search" style={{width: '40px'}}></i>
                 </div>    
-                <DataTable filterDisplay="row" className='dataTable' filters={filters}>
+                <DataTable value={listaClientes} filterDisplay="row" className='dataTable' filters={filters}>
                     <Column style={{ minWidth: '17rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="nome" header="Nome"></Column>
-                    <Column style={{ minWidth: '14rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="telefone" header="Telefone"></Column>
+                    <Column style={{ minWidth: '14rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="email" header="Email"></Column>
                     <Column style={{ minWidth: '15rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="cpf" header="CPF"></Column>
-                    <Column style={{ minWidth: '13rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="born" header="Nascimento"></Column>
-                    <Column style={{ minWidth: '17rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="Adress" header="Endereco"></Column>
-                    <Column style={{ minWidth: '14rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="Gender" header="Gênero"></Column>
+                    <Column style={{ minWidth: '13rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="nascimento" header="Nascimento"></Column>
+                    <Column style={{ minWidth: '17rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="endereco" header="Endereco"></Column>
+                    <Column style={{ minWidth: '14rem', fontSize: '12px' }} showFilterMatchModes={false} filter field="genero" header="Gênero"></Column>
                 </DataTable>
                 <div className='buttonInclude'>
                     <Button onClick={handleOpenModal} style={{backgroundColor: '#85bb65'}} severity='Sucess' size="normal" label="incluir" icon="pi pi-user-plus" />
                 </div>
-            </div>
-            <div>
-               {showModal ? <Modal/> : null }
-            
-            </div>             
-            
+            </div>         
+            {showModal && (<Modal close={()=> setShowModal(false)}/>) }
+   
         </div>
     )
 }
