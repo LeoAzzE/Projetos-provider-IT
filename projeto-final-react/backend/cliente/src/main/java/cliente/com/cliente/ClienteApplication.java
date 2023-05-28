aca4e411853f1ec7a22ef89d9529da2a231c1f3d
Borges dev <leosull50@hotmail.com>
2023-05-28 20:29:37 -0300

    trying to implement authentication

--- projeto-final-react/backend/cliente/src/main/java/cliente/com/cliente/ClienteApplication.java
+++ projeto-final-react/backend/cliente/src/main/java/cliente/com/cliente/ClienteApplication.java
@@ -2,12 +2,14 @@ package cliente.com.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ClienteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteApplication.class, args);
+
 	}
 
 }
